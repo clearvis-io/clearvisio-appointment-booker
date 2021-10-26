586068020f9f910d615fcefb5828cd4807574e38
Hendrich Attila <hendricha@clearvis.io>
2021-10-26 16:47:21 +0200

    style: Fixed responsive style

--- helper/html.js
+++ helper/html.js
@@ -4,6 +4,7 @@ import htm from 'htm';
 var wrappedH = (type, props, ...children) => {
   if (props && props.class) {
     props.class = props.class.trim().split(' ')
+      .filter((className) => className.trim())
       .map((className) => className.indexOf('cvio-ab-') !== 0 ? `cvio-ab-${className}` : className)
       .join(' ');
   }

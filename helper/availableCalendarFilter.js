3058528af591f6399f2d680c17011529b6b25ff7
Hendrich Attila <hendricha@clearvis.io>
2023-02-22 16:28:32 +0100

    helper: availableCalendarFilter now checks process availability for calendars

--- helper/availableCalendarFilter.js
+++ helper/availableCalendarFilter.js
@@ -7,9 +7,24 @@ const roleHierarchy = [
 export default function ({appointment, calendars, calendarRoleCheckMode}) {
   var process = appointment ? appointment.eye_examination_process : null;
 
+  const proccesMatchesCalendarProcessExpectations = function(calendar, process) {
+    if (!calendar.only_specific_processes_allowed) {
+      return true;
+    }
+
+    const splitIri = process['@id'].split('/');
+    const id = parseInt(splitIri[splitIri.length - 1]);
+
+    return calendar.allowed_process_ids.indexOf(id) != -1;
+  }
+
   return calendars.filter(function(calendar) {
     if (!process || !process.highestRequiredRole) {
       return true;
+    }
+
+    if (!proccesMatchesCalendarProcessExpectations(calendar, process)) {
+      return false;
     }
 
     for (let i = 0; i < roleHierarchy.length; i++) {

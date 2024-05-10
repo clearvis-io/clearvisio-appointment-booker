c6c8bf4ba8417c64f5c9e3fe7464b368b6ff2311
Filep-György Dominik <fgydomi@clearvis.io>
2024-05-10 11:01:43 +0200

    Store selection: new variable in reducer and dispatch in index

--- store/storeStore.js
+++ store/storeStore.js
@@ -1,9 +1,13 @@
 import {api} from '../helper/index.js'
 
 export function storeStore (store) {
-  store.on('@init', () => ({ store: null }))
+  store.on('@init', () => ({ store: null , storeSelection: 'no' }))
 
   store.on('store/set', ({ store }, newStore) => {
     return { store: newStore };
+  })
+
+  store.on('store/setStoreSelection/set', (previousValue, storeSelection) => {
+    return { storeSelection };
   })
 }

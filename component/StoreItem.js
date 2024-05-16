7cc5e9166f755490154bc67e2945438c5474eaf8
Filep-György Dominik <fgydomi@clearvis.io>
2024-05-16 18:09:26 +0200

    Address formatter: creating new address formatter, used on storeSelection screen

--- component/StoreItem.js
+++ component/StoreItem.js
@@ -1,10 +1,14 @@
 import {useStoreon} from 'storeon/preact'
-import {html} from '../helper/index.js'
+import {html, addressFormatter} from '../helper/index.js'
 
 export default (props) => {
 
   const { store, dispatch, currentStep } = useStoreon('store', 'currentStep')
 
+  const postal_code = (props.item.postal_code);
+  const city = (props.item.city);
+  const street_address = (props.item.street_address);
+  
   const onClick = () => {
     if (currentStep !== 'storeSelection') {
       return;
@@ -21,6 +25,7 @@ export default (props) => {
           <h4>${props.item.name}</h4>
           <p>${props.item.phone}</p>
           <p>${props.item.email}</p>
+          <p>${addressFormatter.formatAddress({ postal_code, city, street_address })}</p>
         </div>
         <div class="col text-end p-2 next item icon">
           \u203A

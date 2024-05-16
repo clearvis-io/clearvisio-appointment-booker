eab8a0a9516653e99824c7be242165e093a35be7
Filep-György Dominik <fgydomi@clearvis.io>
2024-05-16 13:07:32 +0200

    Click on Store: creating a click action which leads to the next step

--- component/StoreItem.js
+++ component/StoreItem.js
@@ -3,8 +3,19 @@ import {html} from '../helper/index.js'
 
 export default (props) => {
 
+  const { store, dispatch, currentStep } = useStoreon('store', 'currentStep')
+
+  const onClick = () => {
+    if (currentStep !== 'storeSelection') {
+      return;
+    }
+    
+    dispatch('store/set', props.item);
+    dispatch('currentStep/next');
+  }
+
   return html`
-    <li class="row list-group-item">
+  <li class="row list-group-item ${store == props.item ? 'selected' : ''}" onClick=${onClick}>
       <div class="row">
         <div class="col-11 p-0">
           <h4>${props.item.name}</h4>

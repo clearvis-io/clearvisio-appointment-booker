f0bab590bb606e1ef227dd08ed67e525b17329e8
Hevesi Gergő <you@example.com>
2024-05-21 15:11:48 +0200

    store item: added icons for infos

--- component/StoreItem.js
+++ component/StoreItem.js
@@ -13,14 +13,16 @@ export default ({item}) => {
     dispatch('currentStep/next');
   }
 
+  const address = addressFormatter.formatAddress(item);
+
   return html`
-    <li class="row list-group-item ${store == item ? 'selected' : ''}" onClick=${onClick}>
+    <li class="storeItem row list-group-item ${store == item ? 'selected' : ''}" onClick=${onClick}>
       <div class="row">
         <div class="col-11 p-0">
           <h4>${item.name}</h4>
-          <p>${item.phone}</p>
-          <p>${item.email}</p>
-          <p>${addressFormatter.formatAddress(item)}</p>
+          ${item.phone ? html`<span class="iconHolder">\u{1F57B}</span> ${item.phone}<br/>` : ''}
+          ${item.email ? html`<span class="iconHolder">\u{2709}</span> ${item.email}<br/>` : ''}
+          ${address ? html`<span class="iconHolder">\u{1F588}</span> ${address}` : ''}
         </div>
         <div class="col text-end p-2 next item icon">
           \u203A

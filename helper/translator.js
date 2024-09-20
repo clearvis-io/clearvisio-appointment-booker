bc65d02eb938057a0723771b085fcc7f781aae38
Hamrák Mihály <misi@clearvis.io>
2024-09-20 12:08:16 +0200

    Adding RO and HR translation

--- helper/translator.js
+++ helper/translator.js
@@ -225,7 +225,8 @@ const messages = {
     'Error! Appointments can not be created!': 'Eroare! Programarea nu se poate rezerva!',
     'There is a configuration error in the appointment booker that prevents booking. Please contact our store on %mail% email address or book an appointment by calling our store (%tel%) and please mention the error then.':
       'Eroare! Programarea nu se poate rezerva! Rezervarea online a programărilor nu este posibilă din cauza unei erori de configurare în sistemul de rezervare a programărilor. Vă rugăm să contactați magazinul la numărul de telefon %tel% sau adresa de e-mail %email% și să ne raportați această eroare atunci când solicitați o programare prin telefon:',
-    'Store selection': 'Selectare magazin'
+    'Store selection': 'Selectare magazin',
+    'There are no eye examination processes configured in the store network or the length of them is not configured.': 'Nu există protocol de consulație în magazin sau durata protocolului nu este specificat.'
   },
   hr: {
     "Are you sure you want to cancel this appointment?": "Jeste li sigurni da želite otkazati ovaj termin?",
@@ -332,7 +333,8 @@ const messages = {
     'Error! Appointments can not be created!': 'Greška! Rezervacija termina nije moguća!',
     'There is a configuration error in the appointment booker that prevents booking. Please contact our store on %mail% email address or book an appointment by calling our store (%tel%) and please mention the error then.':
       'Online rezervacija termina nije moguća zbog greške u konfiguraciji sustava za rezervaciju termina. Kontaktirajte našu optiku na %tel% telefonski broj ili na %email% email adresu i  prijavite nam ovu pogrešku tijekom telefonskog zahtjeva za termin. ',
-    'Store selection': 'Odabir trgovine'
+    'Store selection': 'Odabir trgovine',
+    'There are no eye examination processes configured in the store network or the length of them is not configured.': 'U optici nije postavljen protokol pregleda ili nije određeno duljina pregleda.'
   },
   en: {
     '@abbrSunday': 'Sun',

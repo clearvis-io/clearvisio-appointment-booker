ec383583031aff169add7dd2faab01b25f225d0d
Hevesi Gergő <you@example.com>
2026-03-13 16:58:36 +0100

    postcss is updated so we have no vulnerability
    and we add the bootstrap license to the css in a plugin...

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require('postcss-import-url'),
+    require('postcss-import'),
     require('postcss-prefixer')({prefix: 'cvio-ab-'}),
     process.env.npm_lifecycle_event == 'dev' ? null : require('cssnano'),
     require('postcss-preset-env'),
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,28 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { resolve, join } from 'path';
+import { readFileSync, writeFileSync } from 'fs';
+
+const bootstrapLicense = `/*!
+ * Bootstrap v5.3.8 (https://getbootstrap.com/)
+ * Copyright 2011-2025 The Bootstrap Authors
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ */
+`;
+
+function addCssLicense() {
+  return {
+    name: 'add-css-license',
+    closeBundle() {
+      const cssFile = join(__dirname, 'dist', 'style.css');
+      try {
+        const css = readFileSync(cssFile, 'utf8');
+        if (!css.startsWith('/*!')) {
+          writeFileSync(cssFile, bootstrapLicense + css);
+        }
+      } catch (e) {}
+    }
+  };
+}
 
 export default defineConfig({
   build: {
@@ -15,6 +38,7 @@ export default defineConfig({
     minify: true,
     target: 'es2018'
   },
+  plugins: [addCssLicense()],
   css: {
     postcss: './postcss.config.js'
   }
